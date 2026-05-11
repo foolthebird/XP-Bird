31ffe9ddb4ebc1393c3834c17e331654c193be23
Johnson <jonteson2@gmail.com>
2026-05-11 11:22:08 +0300

    xp-bird2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 import { useState } from 'react'
 import Signup from './components/Signup'
 import Signin from './components/Signin'
